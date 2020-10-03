1deffff52f685dff1565c4cceb07a6c94bfad506
EC2 Default User <ec2-user@ip-172-31-3-69.ec2.internal>
2020-10-03 05:58:58 +0000

    [finish]task3 modified

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -2,11 +2,16 @@ class BookCommentsController < ApplicationController
 
   def create
     book = Book.find(params[:book_id])
-    comment = BookComment.new(book_comment_params)
-    comment.user_id = current_user.id
-    comment.book_id = book.id
-    comment.save
-    redirect_to book_path(book)
+    @book_comment = BookComment.new(book_comment_params)
+    @book_comment.user_id = current_user.id
+    @book_comment.book_id = book.id
+    if @book_comment.save
+      redirect_to book_path(book)
+    else
+      @user = current_user
+      @book = Book.find(params[:book_id])
+      render "books/show"
+    end
   end
 
 
@@ -14,7 +19,8 @@ class BookCommentsController < ApplicationController
     BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
     redirect_to book_path(params[:book_id])
   end
-
+  
+  private
   def book_comment_params
     params.require(:book_comment).permit(:comment)
   end
